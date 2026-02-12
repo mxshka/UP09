16c3ca1cfc92f4c91f9ebeec31d471268b1870b1
stud <rpradmir3@gmail.com>
2026-02-12 13:56:30 +0300

    Fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,20 @@ import { Hero } from './hero/hero';
 import { Companies } from './companies/companies';
 import { HowItWorks } from './how-it-works/how-it-works';
 import { AiMenu } from './ai-menu/ai-menu';
+import { Step } from "./step/step";
+import { Reciepes } from './reciepes/reciepes';
+import { Reviews } from './reviews/reviews';
+import { Price } from './price/price';
+import { Form } from './form/form';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Hero, Companies, HowItWorks, AiMenu],
+  imports: [RouterOutlet, Header, 
+    Hero, Companies, HowItWorks, 
+    AiMenu, Step, Reciepes, Reviews, 
+    Price, Form, Footer],
   templateUrl: 'app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/reciepes/reciepes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reciepes',
+  imports: [],
+  templateUrl: './reciepes.html',
+  styleUrl: './reciepes.css',
+})
+export class Reciepes {
+
+}
